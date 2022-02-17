163c9f4980c33a4e77c776abda2e8cfc71ec7dc4
Vigneshps27 <98878153+Vigneshps27@users.noreply.github.com>
2022-02-17 21:01:46 +0530

    Update digitalclock.c

--- 3_Implementation/digitalclock.c
+++ 3_Implementation/digitalclock.c
@@ -1,35 +1,5 @@
 #include<stdio.h>
 #include "digitalclock.h"
-int main()
-{
-int input;
-printf("\n\t\t\t---Welcome To Digital Clock---\n\n\n");
-
-    printf( "\t\t1. Current Time\n\n" );
-    printf( "\t\t2. Stop Watch\n\n" );
-    printf( "\t\t3. Set Time\n\n" );
-    printf( "\t\t4. Exit\n\n" );
-    printf( "\t\tSelection: " );
-    scanf( "%d", &input );
-    switch ( input ) {
-        case 1:
-            current_time();
-            break;
-        case 2:
-            stop_watch();
-            break;
-        case 3:
-            set_time();
-            break;
-        case 4:
-            printf( "Thanks for using the clock\n" );
-            break;
-        default:
-            printf( "Bad input, quitting!\n" );
-            break;
-    }
-    return 0;
-}
 int current_time()
 {
     time_t s;
@@ -131,6 +101,36 @@ int set_time()
 }
 return 0;
 
+}
+int main()
+{
+int input;
+printf("\n\t\t\t---Welcome To Digital Clock---\n\n\n");
+
+    printf( "\t\t1. Current Time\n\n" );
+    printf( "\t\t2. Stop Watch\n\n" );
+    printf( "\t\t3. Set Time\n\n" );
+    printf( "\t\t4. Exit\n\n" );
+    printf( "\t\tSelection: " );
+    scanf( "%d", &input );
+    switch ( input ) {
+        case 1:
+            current_time();
+            break;
+        case 2:
+            stop_watch();
+            break;
+        case 3:
+            set_time();
+            break;
+        case 4:
+            printf( "Thanks for using the clock\n" );
+            break;
+        default:
+            printf( "Bad input, quitting!\n" );
+            break;
+    }
+    return 0;
 }
 
 
