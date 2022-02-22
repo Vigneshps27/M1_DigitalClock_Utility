41383af00431cfbf2520f7b3945d7d37d289bf41
Vigneshps27 <98878153+Vigneshps27@users.noreply.github.com>
2022-02-22 10:42:47 +0530

    Create set_time.c

---
+++ 3_Implementation/set_time.c
@@ -0,0 +1,44 @@
+int set_time()
+{
+    int hour,minute,second;
+    int D = 1000;
+    printf("Set time in 24 Hours format (HH:MM:SS):\n");
+    scanf("%d:%d:%d", &hour, &minute, &second);
+    if (hour > 23)
+        {
+            printf("ERROR!\n");
+            exit(0);
+        }
+    if (minute > 60)
+        {
+             printf("ERROR!\n");
+             exit(0);
+        }
+    if (second > 60)
+        { printf("ERROR!\n");
+        exit(0);
+        }
+    while (1)
+        {
+            second += 1;
+            if (second > 59)
+                { minute += 1;
+                  second = 0;
+                }
+            if (minute > 59)
+                { hour += 1;
+                  minute = 0;
+                }
+            if (hour > 23)
+                { hour = 0;
+                  minute = 0;
+                  second = 0;
+                }
+    printf("\n Clock\n");
+    printf("\n%02d:%02d:%02d", hour, minute, second);
+    sleep(D);
+    system("clear");
+
+}
+return 0;
+}
